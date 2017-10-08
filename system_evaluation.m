f845443b618930beb14f0ec53f6413a4987ec6dc
Marc Górriz Blanch <gbmarc@hotmail.es>
2017-10-08 20:48:31 +0200

    Update system_evaluation.m

--- system_evaluation.m
+++ system_evaluation.m
@@ -17,17 +17,17 @@ function system_evaluation(directory)
         for i=1:size(val_dataset{1},1)
             file_id=val_dataset{1}(i);
             gt = imread(strcat(directory, '/mask/mask.', file_id{1}, '.png'));
-            mask = imread(strcat('candidate_mask/mask.0',num2str(technique),'.', file_id{1$
+            mask = imread(strcat('candidate_mask/mask.0',num2str(technique),'.', file_id{1}, '.png'));
 
             [pixelTP, pixelFP, pixelFN, pixelTN] = PerformanceAccumulationPixel(mask, gt);
             [pixelPrecision, pixelAccuracy, pixelSpecificity, pixelSensitivity] = ...
                 PerformanceEvaluationPixel(pixelTP, pixelFP, pixelFN, pixelTN);
 
-            evaluation = [evaluation; [pixelPrecision, pixelAccuracy, pixelSpecificity, pi$
+            evaluation = [evaluation; [pixelPrecision, pixelAccuracy, pixelSpecificity, pixelSensitivity]];
         end
 
         tech_eval = mean(evaluation);
-        fprintf(eval_file, '%d\t%f\t%f\t%f\t%f\n', technique, tech_eval(1),tech_eval(2), t$
+        fprintf(eval_file, '%d\t%f\t%f\t%f\t%f\n', technique, tech_eval(1),tech_eval(2), tech_eval(3), tech_eval(4));
     end
     fclose(eval_file);
 
