8682c73c2260272a8154761f2a5b8ed2b2895d52
Marc Górriz Blanch <gbmarc@hotmail.es>
2017-10-08 20:40:55 +0200

    System evaluation

---
+++ system_evaluation.m
@@ -0,0 +1,34 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%% SYSTEM EVALUATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function system_evaluation(directory)
+
+    addpath('evaluation/');
+
+    %Evaluation results file
+    eval_file = fopen('eval_results.txt', 'w');
+    val_dataset = textscan(fopen('val_dataset.txt','rt'),'%s');
+
+    %Evaluation per technique
+    for technique=1:3
+        evaluation = [];
+
+        for i=1:size(val_dataset{1},1)
+            file_id=val_dataset{1}(i);
+            gt = imread(strcat(directory, '/mask/mask.', file_id{1}, '.png'));
+            mask = imread(strcat('candidate_mask/mask.0',num2str(technique),'.', file_id{1$
+
+            [pixelTP, pixelFP, pixelFN, pixelTN] = PerformanceAccumulationPixel(mask, gt);
+            [pixelPrecision, pixelAccuracy, pixelSpecificity, pixelSensitivity] = ...
+                PerformanceEvaluationPixel(pixelTP, pixelFP, pixelFN, pixelTN);
+
+            evaluation = [evaluation; [pixelPrecision, pixelAccuracy, pixelSpecificity, pi$
+        end
+
+        tech_eval = mean(evaluation);
+        fprintf(eval_file, '%d\t%f\t%f\t%f\t%f\n', technique, tech_eval(1),tech_eval(2), t$
+    end
+    fclose(eval_file);
+
+end
