d66183fd655cce499ae6387953b997ab55cf25ec
fran <fran.roldans@gmail.com>
2017-10-15 16:26:56 +0200

    week2 folder added

---
+++ week2/launch_BackProjectionSegmentation.m
@@ -0,0 +1,6 @@
+input_directory = '/home/mcv00/DataSet/4c/train';
+output_directory = 'BackProj_Masks';
+
+BackProjectionSegmentation(input_directory, output_directory);
+
+
---
+++ week2/launch_back_projection_segmentation.m
@@ -0,0 +1,2 @@
+[perceptual_info] = BackProjectionTrain('/home/mcv00/DataSet/4c/train', 32);
+BackProjectionSegmentation('/home/mcv00/DataSet/4c/train', 'BackProjMasks', perceptual_info, 0.25);
