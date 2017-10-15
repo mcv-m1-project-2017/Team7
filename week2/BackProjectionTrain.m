8b35cd91d48faf27cf5cbe3337f029abd3bd1757
Marc Górriz Blanch <gbmarc@hotmail.es>
2017-10-15 20:28:06 +0200

    Update BackProjectionTrain.m

--- week2/BackProjectionTrain.m
+++ week2/BackProjectionTrain.m
@@ -14,17 +14,15 @@ function [perceptual_info] = BackProjectionTrain(directory, bins)
     % The function returns the perceptual information from each group.
     % perceptual_info = {G1, G2, G3} being 'Gx' a struct to index the 2D
     % perceptual histogram between Hue and Saturation components.
-    
-    train_dataset = textscan(fopen('train_dataset.txt','rt'),'%s');
-    px_coordinates = textscan(fopen('px_coordinates.txt', 'rt'), ...
-        '%s\t%d\t%d\t%d\t%d\t%c\n');
+   
+    train_dataset = txt2cell('train_dataset.txt', 'columns', [1, 9, 10, 11, 12, 13]);
 
     g1 = 'ABC'; g2 = 'DF'; g3 = 'E';
     g1_signals = []; g2_signals = []; g3_signals = [];
 
-    for i=1:size(train_dataset{1},1)
+    for i=1:size(train_dataset,1)
 
-        file_id=train_dataset{1}(i);
+        file_id=train_dataset(i,1);
         im = imread(strcat(directory,'/',file_id{1},'.jpg'));
         im_hsv = rgb2hsv(im);
         
@@ -32,40 +30,34 @@ function [perceptual_info] = BackProjectionTrain(directory, bins)
         r_pixels=(im(:,:,2)*1.2<im(:,:,1) & im(:,:,3)*1.2<im(:,:,1));
         b_pixels=(im(:,:,1)*1.2<im(:,:,3) & im(:,:,2)*1.2<im(:,:,3));
         
-        %Find ground truth information for file_id
-        pos = find(strcmp(px_coordinates{1},file_id{1}));
-        
-        for j=1:length(pos)
             
-            coordinates = {px_coordinates{2}(pos(j)), px_coordinates{3}(pos(j)), ...
-            px_coordinates{4}(pos(j)), px_coordinates{5}(pos(j)), px_coordinates{6}(pos(j))};
-        
-            mask = r_pixels(coordinates{1}:coordinates{3},coordinates{2}:coordinates{4})...
-                | b_pixels(coordinates{1}:coordinates{3},coordinates{2}:coordinates{4});
-            mask = reshape(mask,[size(mask,1)*size(mask,2),1]);
-            
-            pixels_h = im_hsv(coordinates{1}:coordinates{3},coordinates{2}:coordinates{4},1);
-            pixels_h = reshape(pixels_h,[size(pixels_h,1)*size(pixels_h,2),1]);
-            pixels_h = pixels_h(mask==1);
-            
-            pixels_s = im_hsv(coordinates{1}:coordinates{3},coordinates{2}:coordinates{4},2);
-            pixels_s = reshape(pixels_s,[size(pixels_s,1)*size(pixels_s,2),1]);
-            pixels_s = pixels_s(mask==1);
+        coordinates = {str2double(train_dataset(i,2)), str2double(train_dataset(i,3)), ...
+            str2double(train_dataset(i,4)), str2double(train_dataset(i,5))};
 
-            if ~isempty(strfind(g1, coordinates{5}))
-                %group 1 signals: red
-                g1_signals = [g1_signals;[pixels_h, pixels_s]];
+        mask = r_pixels(coordinates{1}:coordinates{3},coordinates{2}:coordinates{4}) ...
+            | b_pixels(coordinates{1}:coordinates{3},coordinates{2}:coordinates{4});
+        mask = reshape(mask,[size(mask,1)*size(mask,2),1]);
 
-            elseif ~isempty(strfind(g2, coordinates{5}))
-                %group 2 signals: blue
-                g2_signals = [g2_signals;[pixels_h, pixels_s]];
-                
-            elseif ~isempty(strfind(g3, coordinates{5}))
-                %group 3 signals: red & blue
-                g3_signals = [g3_signals;[pixels_h, pixels_s]];
+        pixels_h = im_hsv(coordinates{1}:coordinates{3},coordinates{2}:coordinates{4},1);
+        pixels_h = reshape(pixels_h,[size(pixels_h,1)*size(pixels_h,2),1]);
+        pixels_h = pixels_h(mask==1);
 
-            end
-            
+        pixels_s = im_hsv(coordinates{1}:coordinates{3},coordinates{2}:coordinates{4},2);
+        pixels_s = reshape(pixels_s,[size(pixels_s,1)*size(pixels_s,2),1]);
+        pixels_s = pixels_s(mask==1);
+
+        if contains(g1, train_dataset(i,6))
+            %group 1 signals: red
+            g1_signals = [g1_signals;[pixels_h, pixels_s]];
+
+        elseif contains(g2, train_dataset(i,6))
+            %group 2 signals: blue
+            g2_signals = [g2_signals;[pixels_h, pixels_s]];
+
+        elseif contains(g3, train_dataset(i,6))
+            %group 3 signals: red & blue
+            g3_signals = [g3_signals;[pixels_h, pixels_s]];
+
         end
         
     end
