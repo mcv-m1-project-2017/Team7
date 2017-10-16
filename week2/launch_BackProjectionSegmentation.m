2435fe4f7dde1205644c76bc288ea83fdf49c60f
Marc Górriz Blanch <gbmarc@hotmail.es>
2017-10-16 17:00:15 +0200

    Delete launch_BackProjectionSegmentation.m

--- week2/launch_BackProjectionSegmentation.m
+++
@@ -1,7 +0,0 @@
-[perceptual_info] = BackProjectionTrain('/home/mcv00/DataSet/4c/train', 32);
-
-%Generate masks without noise reduction
-BackProjectionSegmentation('/home/mcv00/DataSet/4c/train', 'BackProjMasks', perceptual_info, 0.25, 0);
-
-%Generate masks with noise reduction
-BackProjectionSegmentation('/home/mcv00/DataSet/4c/train', 'BackProjMasks', perceptual_info, 0.25, 1);
